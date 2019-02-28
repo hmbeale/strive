80376eb89108cd05dda8ff6ebbf8dd8fd071d2c0
Holden Beale <itdbealh@aikencounty.net>
2019-02-28 14:21:46 -0500

    even more scenery

--- strive.js
+++ strive.js
@@ -41,8 +41,8 @@ scenery.adjective = '';
 scenery.type = '';
 
 const sceneryAdjectives = ['small', 'big', 'dark', 'sunlit'];
-const sceneryTypes = ['river', 'pool of water', 'tree',
-                      'tower', 'hill', 'cliff', 'cloud'];
+const sceneryTypes = ['river', 'pool of water', 'tree', 'cluster of bushes',
+                      'tower', 'town in the distance', 'flock of birds', 'cloud'];
 
 const distNeeded = 30;
 
